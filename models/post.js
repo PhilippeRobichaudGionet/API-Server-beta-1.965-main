34df4107d71607e8d5be05957468c92724236190
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-10-31 10:20:07 -0400

    lol

--- models/post.js
+++ models/post.js
@@ -2,13 +2,12 @@ import Model from './model.js';
 
 export default class Post extends Model {
     constructor() {
-        super();
-        this.addField('Id','string')
+        super(true);
+
         this.addField('Title', 'string');
         this.addField('Text', 'string');
         this.addField('Category', 'string');
         this.addField('Image', 'asset');
         this.addField('Creation', 'integer');
-        this.setKey("Id");
     }
 }
