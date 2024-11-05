968c6a46c857f608bb2460c24f3f49762d7b7431
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-11-05 11:31:35 -0500

    Modify CSS element et script

--- wwwroot/Posts/JS/script.js
+++ wwwroot/Posts/JS/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-    
+    let EditBtn = $("#Edit");
+    let DeleteBtn = $("#Delete");
+
+
+
+
+
+    EditBtn.click(function (e) { 
+       console.log("Edit");
+        
+    });
+
+    DeleteBtn.click(function (e) { 
+        console.log("Delete");
+        
+    });
 });
