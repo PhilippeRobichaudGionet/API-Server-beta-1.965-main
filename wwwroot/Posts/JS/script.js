d2565a899cb54a4edc5815f1f5058ed2cac902e2
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-11-12 11:25:42 -0500

    change css

--- wwwroot/Posts/JS/script.js
+++ wwwroot/Posts/JS/script.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
     let EditBtn = $("#Edit");
     let DeleteBtn = $("#Delete");
+    let NewZone = $("#News");
+    let BtnSection = $("#BtnSection");
 
-
-
-
+    // Show/Hide Button
+    NewZone.hover(function () { BtnSection.show(); }, function () { BtnSection.hide(); });
 
     EditBtn.click(function (e) { 
-       console.log("Edit");
-        
+       console.log("Edit");    
     });
 
     DeleteBtn.click(function (e) { 
-        console.log("Delete");
-        
+        console.log("Delete");   
     });
 });
