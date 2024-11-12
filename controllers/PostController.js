99d6296da24d9a0e79b759c4e680ab7abe328b18
MrWelz <thierrybecker28@gmail.com>
2024-11-12 10:57:32 -0500

    hyjkvfkhvg

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -16,24 +16,31 @@ export default
         return false;
     }
     get() {
-        if (this.HttpContext.path.queryString != undefined)
+        if (this.HttpContext.path.queryString != '')
             this.getFilteredPosts();
+        else
+            this.list();
     }
+    list() {
+            this.HttpContext.response.JSON(
+                this.repository.getAll(this.HttpContext.path.params, this.repository.ETag)
+            );
+        }
     getFilteredPosts() {
-        const { Title, Text, Category } = this.params;
-
-        let posts = this.repository.getAll();
-        if (Title || Text) {
-            const MotFiltre = (Title || Text).toLowerCase();
+        const { title, text, category } = this.params;
+        let posts = this.repository.getAll(this.HttpContext.path.params, this.repository.ETag);
+        // Filtrage par mot-clé dans le titre ou texte
+        if (title || text) {
+            const MotFiltre = (title || text).toLowerCase();
             posts = posts.filter(post =>
                 (post.Title && post.Title.toLowerCase().includes(MotFiltre)) ||
                 (post.Text && post.Text.toLowerCase().includes(MotFiltre))
             );
         }
-        if (Category) {
-            posts = posts.filter(post => post.Category === Category);
+        if (category) {
+            posts = posts.filter(post => post.category === category);
         }
-        if (posts.length > 0) {
+        if (posts!= null &&posts.length > 0 ) {
             this.HttpContext.response.JSON(posts);
         } else {
             this.error("Aucun post ne correspond aux paramètres.");
